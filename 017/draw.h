0c9c47ed80638da11f23a4986290588155e16317
David Parker <davidwparker@gmail.com>
2011-11-23 17:04:21 -0700

    modified git ignore

--- 017/draw.h
+++ 017/draw.h
@@ -2,8 +2,4 @@ void drawAxes(void);
 void drawLight(void);
 void drawParameters(void);
 void drawScene(void);
-void drawScreencast11(void);
-void drawScreencast12(void);
-void drawScreencast13(void);
-void drawScreencast14(void);
-void drawScreencast15(void);
+
